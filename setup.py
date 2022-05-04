f1ad6b47729951a15f08b94bc69553c4d1941f6b
Henrik Palmlund Wahlgren <henrik@pwit.se>
2022-05-04 11:33:27 +0200

    updated version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ PROJECT_URLS = {
 EMAIL = "henrik.wahlgren@utilitarian.io"
 AUTHOR = "Henrik Palmlund Wahlgren @ Palmlund Wahlgren Innovative Technology AB"
 REQUIRES_PYTHON = "~=3.7"
-VERSION = "22.2.0"
+VERSION = "22.2.1"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
